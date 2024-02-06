8826fbb82dd022d7b710c748dd5c26facde8bd48
Wyclif03 <ppierson06@hotmail.com>
2024-02-06 14:10:09 +0000

    small compoent changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Dashboard />
+      <p>hello</p>
     </main>
   );
 }
