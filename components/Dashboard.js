01ac98c6b1634547632a1112132b8a94a68e59c9
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-02-08 12:03:40 +0000

    styled poroject tiles

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -10,23 +10,23 @@ import axios from 'axios'
 
 const Dashboard = () => {
 
-const [posts, setPosts] = useState([]);
+// const [posts, setPosts] = useState([]);
 
-useEffect(() => {
-  // Fetch posts
-  axios.get('http://localhost:5000/')
-  .then(res => {
-    const { data } = res.data;
-    setPosts(data);
-    console.log(data)
+// useEffect(() => {
+//   // Fetch posts
+//   axios.get('http://localhost:5000/')
+//   .then(res => {
+//     const { data } = res.data;
+//     setPosts(data);
+//     console.log(data)
   
-  })
+//   })
   
-  .catch(error => {
-    console.error('Error fetching books:', error);
-  });
+//   .catch(error => {
+//     console.error('Error fetching books:', error);
+//   });
   
-}, []) 
+// }, []) 
 
 
 
@@ -34,7 +34,30 @@ useEffect(() => {
 
 return (
   <>
-    {posts.map(post => (
+ <div className="flex w-full">
+       <div className="w-1/5">
+        <Sidebar />
+       </div>
+     <div className="w-4/5 space-y-4">
+       <div className="p-4 bg-white shadow rounded">
+          <ProjectCard />
+         </div>
+         <div className="p-4 bg-gray-200 shadow rounded">
+           <MakePost />
+         </div>
+         <div className="p-4 bg-gray-300 shadow rounded">
+           <FullPost />
+         </div>
+         <div className="p-4 bg-gray-300 shadow rounded">
+           <Login />
+         </div>
+         <div className="p-4 bg-gray-300 shadow rounded">
+           <Signup />
+         </div>
+       </div>
+     </div>
+  
+    {/* {posts.map(post => (
       <div key={post._id}>
         <h2>{post.name}</h2>
         <p>Description: {post.description}</p>
@@ -42,31 +65,10 @@ return (
         <p>Repo Link: {post.repoLink}</p>
         <p>Timeframe: {post.timeframe}</p>
       </div>
-    ))}
+    ))} */}
   </>
 
-    // <div className="flex w-full">
-    //   <div className="w-1/5">
-    //     <Sidebar />
-    //   </div>
-    //   <div className="w-4/5 space-y-4">
-    //     <div className="p-4 bg-white shadow rounded">
-    //       <ProjectCard />
-    //     </div>
-    //     <div className="p-4 bg-gray-200 shadow rounded">
-    //       <MakePost />
-    //     </div>
-    //     <div className="p-4 bg-gray-300 shadow rounded">
-    //       <FullPost />
-    //     </div>
-    //     <div className="p-4 bg-gray-300 shadow rounded">
-    //       <Login />
-    //     </div>
-    //     <div className="p-4 bg-gray-300 shadow rounded">
-    //       <Signup />
-    //     </div>
-    //   </div>
-    // </div>
+    
   )
 }
 
