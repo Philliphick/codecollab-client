21908c870cb0b3bd28a42ac65f6e21dfaea3bdfa
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-02-13 14:23:22 +0000

    page.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -7,26 +7,27 @@ import Sidebar from './Sidebar'
 import Login from './Login'
 import Signup from './Signup'
 import axios from 'axios'
+import ProtectedPage from './ProtectedPage'
 
 const Dashboard = () => {
 
-// const [posts, setPosts] = useState([]);
+const [posts, setPosts] = useState([]);
 
-// useEffect(() => {
-//   // Fetch posts
-//   axios.get('http://localhost:5000/')
-//   .then(res => {
-//     const { data } = res.data;
-//     setPosts(data);
-//     console.log(data)
+useEffect(() => {
+  // Fetch posts
+  axios.get('http://localhost:5000/')
+  .then(res => {
+    const { data } = res.data;
+    setPosts(data);
+    console.log(data)
   
-//   })
+  })
   
-//   .catch(error => {
-//     console.error('Error fetching books:', error);
-//   });
+  .catch(error => {
+    console.error('Error fetching books:', error);
+  });
   
-// }, []) 
+}, []) 
 
 
 
@@ -37,6 +38,7 @@ return (
  <div className="flex w-full">
        <div className="w-1/5">
         <Sidebar />
+        <ProtectedPage />
        </div>
      <div className="w-4/5 space-y-4">
        <div className="p-4 bg-white shadow rounded">
